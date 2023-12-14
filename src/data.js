14c6cc837f9783aab5e729d4603f98c45959cbe6
debian-pc <mmekraz781@proton.me>
2023-12-13 19:08:28 -0500

    created name data.js & render component

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const nameList = [
+  { id: 1, name: "Muneer" },
+  { id: 2, name: "Emily" },
+  { id: 3, name: "Jacob" },
+  { id: 4, name: "Erin" },
+  { id: 5, name: "Michael" },
+  { id: 6, name: "Sarah" },
+];
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag";
+import { nameList } from "./data";
 
 import "./styles.css";
 
-const App = () => (
-  <div className="App">
-    <h1>Name Tag Generator</h1>
-    <NameTag name="Muneer" />
-  </div>
-);
+const renderNameTag = (names) => <NameTag key={names.id} name={names.name} />;
+
+const App = () => {
+  const NameTagElements = nameList.map(renderNameTag);
+
+  return (
+    <div className="App">
+      <h1>Name Tag Generator</h1>
+      {NameTagElements}
+    </div>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
