66041f5b231a292d239d466b8ab5da2b15f5b303
debian-pc <mmekraz781@proton.me>
2023-12-16 14:56:00 -0500

    Changed functional comp to stateful comp

--- src/data.js
+++
@@ -1,8 +0,0 @@
-export const nameList = [
-  { id: 1, name: "Muneer" },
-  { id: 2, name: "Emily" },
-  { id: 3, name: "Jacob" },
-  { id: 4, name: "Erin" },
-  { id: 5, name: "Michael" },
-  { id: 6, name: "Sarah" },
-];
--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag";
-import { nameList } from "./data";
 
 import "./styles.css";
 
-const renderNameTag = (names) => <NameTag key={names.id} name={names.name} />;
+const renderNameTag = (names) => <NameTag key={names} name={names} />;
 
-const App = () => {
-  const NameTagElements = nameList.map(renderNameTag);
-
-  return (
-    <div className="App">
-      <h1>Name Tag Generator</h1>
-      {NameTagElements}
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    nameList: ["Muneer", "Emily", "Jacob", "Erin", "Michael", "Sarah"],
+  };
+  render() {
+    const NameTagElements = this.state.nameList.map(renderNameTag);
+    return (
+      <div className="App">
+        <h1>Name Tag Generator</h1>
+        {NameTagElements}
+      </div>
+    );
+  }
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
